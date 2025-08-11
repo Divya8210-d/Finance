b3dfc879bc8768d113956c02c34fa429ae4b7806
Divya8210-d <divyanshuchoubey8210@gmail.com>
2025-08-11 22:51:56 +0530

    backend change

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ import paymentRoutes from './routes/payment.routes.js';
 const app = express();
 
 
-const allowedOrigins = ['https://finanlytic.vercel.app', 'http://localhost:5173'];
+const allowedOrigins = ['https://finanlytic.vercel.app', 'http://localhost:5173','http://localhost:5174'];
 
 app.use(cors({
   origin: function (origin, callback) {
